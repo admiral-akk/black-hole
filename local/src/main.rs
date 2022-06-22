7668a432d27b416d3917978371520b429754d194
Kuba Karpierz <mr.karpierz@gmail.com>
2022-06-22 11:17:22 -0400

    Broke image rendering up into multiple functions

--- local/src/main.rs
+++ local/src/main.rs
@@ -3,21 +3,25 @@ use std::path::Path;
 
 use rendering::{init, structs::dimensions::Dimensions};
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
-
     let mut file_name: String = "image.png".to_string();
-
-    if args.len() > 2 {
-        file_name = format!("{}.png", args[2]);
-    }
     let mut dimensions = Dimensions::new(200, 100);
 
+    set_up(&mut file_name, &mut dimensions);
+    render_image(file_name, dimensions);
+}
+
+fn set_up(file_name: &mut String, dimensions: &mut Dimensions) {
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 2 {
+        *file_name = format!("{}.png", args[2]);
+    }
     if args.len() == 5 {
         dimensions.width = str::parse(&args[3]).unwrap();
         dimensions.height = str::parse(&args[4]).unwrap();
     }
+}
 
+fn render_image(file_name: String, dimensions: Dimensions) {
     let renderer = init(dimensions.clone());
     let mut buffer: Vec<u8> = vec![0; 4 * dimensions.size()];
     renderer.render(&mut buffer, &dimensions);
