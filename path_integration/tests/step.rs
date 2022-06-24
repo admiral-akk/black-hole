112553aa05969624bfad8e304c90c7c6b50ac4a8
Kuba Karpierz <mr.karpierz@gmail.com>
2022-06-24 13:53:54 -0400

    refactored ploting to be reusable

--- path_integration/tests/step.rs
+++ path_integration/tests/step.rs
@@ -12,10 +12,41 @@ mod tests {
         style::{Color, IntoFont, ShapeStyle, BLACK, GREEN, RED, WHITE},
     };
 
+    fn trim_path(x_min: f32, x_max: f32, y_min: f32, y_max: f32, path: &Vec<Vec3>) -> Vec<Vec3> {
+        let mut trim_path = Vec::new();
+        for i in 0..path.len() {
+            let (x, y) = (path[i].x, path[i].y);
+            if x < x_min || x > x_max || y < y_min || y > y_max {
+                let (prev_x, prev_y) = (path[i - 1].x, path[i - 1].y);
+                let (final_x, final_y) = (path[i].x, path[i].y);
+                let (slope_x, slope_y) = (final_x - prev_x, final_y - prev_y);
+                let mut t = 1.0;
+                if final_x < x_min {
+                    t = f32::min((x_min - prev_x) / slope_x, t);
+                }
+                if final_x > x_max {
+                    t = f32::min((x_max - prev_x) / slope_x, t);
+                }
+                if final_y < y_min {
+                    t = f32::min((y_min - prev_y) / slope_y, t);
+                }
+                if final_y > y_max {
+                    t = f32::min((y_max - prev_y) / slope_y, t);
+                }
+                trim_path.pop();
+                trim_path.push(Vec3::new(prev_x + t * slope_x, prev_y + t * slope_y, 0.0));
+                break;
+            } else {
+                trim_path.push(path[i]);
+            }
+        }
+        trim_path
+    }
+
     fn plot_trajectories(
         field_scale: f32,
         field_center: &Vec3,
-        lines: u32,
+        lines: &Vec<Vec<Vec3>>,
     ) -> Result<(), Box<dyn std::error::Error>> {
         let path = format!("output/test_{}_paths.png", field_scale);
         let mut root = BitMapBackend::new(&path, (640, 480)).into_drawing_area();
@@ -42,44 +73,12 @@ mod tests {
             },
         ))?;
 
-        for x in 0..lines {
-            let r = (x as f32) / ((lines as f32) - 1.0);
-
-            let end = Vec3::new(10.0 * r, 10.0, 0.0);
-            let ray = Ray::new(start, end - start);
-            let steps = cast_ray_steps_debug(&ray, field_scale, &field_center);
-
-            let mut debug = Vec::new();
-            for i in 0..steps.len() {
-                let (x, y) = (steps[i].x, steps[i].y);
-                if x < 0.0 || x > 10.0 || y < 0.0 || y > 10.0 {
-                    let (prev_x, prev_y) = (steps[i - 1].x, steps[i - 1].y);
-                    let (final_x, final_y) = (x, y);
-                    let (slope_x, slope_y) = (final_x - prev_x, final_y - prev_y);
-                    let mut t = 1.0;
-                    if final_x < 0.0 {
-                        t = f32::min(-prev_x / slope_x, t);
-                    }
-                    if final_x > 10.0 {
-                        t = f32::min((10.0 - prev_x) / slope_x, t);
-                    }
-                    if final_y < 0.0 {
-                        t = f32::min(-prev_y / slope_y, t);
-                    }
-                    if final_y > 10.0 {
-                        t = f32::min((10.0 - prev_y) / slope_y, t);
-                    }
-                    debug.push((prev_x + slope_x * t, prev_y + slope_y * t));
-                    break;
-                }
-                debug.push((x, y));
-            }
+        for i in 0..lines.len() {
+            let r = (i as f32) / ((lines.len() as f32) - 1.0);
+            let path = &lines[i];
+            let trim = trim_path(0.0, 10.0, 0.0, 10.0, path);
             let mut color = RGBColor((255.0 * r) as u8, (255.0 * (1.0 - r)) as u8, 0);
-            if cast_ray_steps(&ray, field_scale, field_center).is_none() {
-                color = BLACK;
-            }
-
-            chart.draw_series(LineSeries::new(debug, &color))?;
+            chart.draw_series(LineSeries::new(trim.iter().map(|v| (v.x, v.y)), &color))?;
         }
         // And if we want SVG backend
         // let backend = SVGBackend::new("output.svg", (800, 600));
@@ -107,14 +106,26 @@ mod tests {
 
     #[test]
     fn plot_all_trajectories() -> Result<(), Box<dyn std::error::Error>> {
+        let start = Vec3::new(5.0, 0.0, 0.0);
         for scale in 0..=10 {
-            plot_trajectories((scale as f32) / 10.0, &Vec3::new(5.0, 5.0, 0.0), 100)?;
+            let (field_scale, field_center) = ((scale as f32) / 10.0, &Vec3::new(5.0, 5.0, 0.0));
+            let mut lines: Vec<Vec<Vec3>> = Vec::new();
+            for i in 0..100 {
+                let r = (i as f32) / 99.0;
+                let end = Vec3::new(10.0 * r, 10.0, 0.0);
+                let ray = Ray::new(start, end - start);
+                let path = cast_ray_steps_debug(&ray, field_scale, &field_center);
+                lines.push(path);
+            }
+            plot_trajectories((scale as f32) / 10.0, &Vec3::new(5.0, 5.0, 0.0), &lines)?;
         }
         Ok(())
     }
 
     #[test]
     fn plot_near_trajectory() -> Result<(), Box<dyn std::error::Error>> {
+        let field_center = Vec3::new(5.0, 5.0, 0.0);
+        let (left, right) = find_near_miss(0.1, &field_center);
         Ok(())
     }
 }
