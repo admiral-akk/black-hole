b13ad768a4d1006175d682ced580684c2b70a9e3
Kuba Karpierz <mr.karpierz@gmail.com>
2022-06-24 12:46:45 -0400

    Added adaptive stepsize

--- path_integration/src/lib.rs
+++ path_integration/src/lib.rs
@@ -4,15 +4,33 @@ use particle::Particle;
 mod particle;
 
 fn field(pos: &Vec3, force_scale: f32, field_center: &Vec3) -> Vec3 {
-    let diff = *pos - *field_center;
+    let diff = *field_center - *pos;
 
     force_scale * diff.normalize() / diff.length().powi(5)
 }
 
-fn step_particle(particle: &mut Particle, force_scale: f32, field_center: &Vec3, h: f32) {
+fn step_particle(particle: &mut Particle, force_scale: f32, field_center: &Vec3) {
+    let h = step_size(particle, force_scale, field_center);
     particle.v += h * field(&particle.p, force_scale, field_center);
     particle.p += h * particle.v;
 }
+
+fn step_size(particle: &mut Particle, force_scale: f32, field_center: &Vec3) -> f32 {
+    let diff = particle.p - *field_center;
+    let r = diff.length();
+    let is_facing = particle.v.normalize().dot(diff) >= 0.0;
+
+    if r > 4.0 * force_scale {
+        if is_facing {
+            return 0.1 * r * r;
+        } else {
+            return 0.1 * (r - 4.0 * force_scale) + 0.001;
+        }
+    } else {
+        return 0.001;
+    }
+}
+
 fn runge_kutta(h: f32, t_n: f32, y_n: Vec3, k1: Vec3, k2: Vec3, k3: Vec3, k4: Vec3) -> (f32, Vec3) {
     (
         t_n + h,
@@ -24,7 +42,7 @@ fn runge_kutta(h: f32, t_n: f32, y_n: Vec3, k1: Vec3, k2: Vec3, k3: Vec3, k4: Ve
 pub fn cast_ray_steps(ray: &Ray, force_scale: f32, field_center: &Vec3) -> Vec3 {
     let mut particle = Particle::new(ray.pos, ray.dir);
     for _ in 0..10 {
-        step_particle(&mut particle, force_scale, field_center, 0.001);
+        step_particle(&mut particle, force_scale, field_center);
     }
 
     particle.v
@@ -36,7 +54,10 @@ pub fn cast_ray_steps_debug(ray: &Ray, force_scale: f32, field_center: &Vec3) ->
     let mut steps = Vec::new();
     for _ in 0..10000 {
         steps.push(particle.p);
-        step_particle(&mut particle, force_scale, field_center, 0.01);
+        if (particle.p - *field_center).length() < 0.15 {
+            return steps;
+        }
+        step_particle(&mut particle, force_scale, field_center);
     }
     steps.push(particle.p);
 
--- path_integration/tests/step.rs
+++ path_integration/tests/step.rs
@@ -12,16 +12,16 @@ mod tests {
         style::{Color, IntoFont, ShapeStyle, BLACK, GREEN, RED, WHITE},
     };
 
-    #[test]
-    fn plot_straight_trajectories() -> Result<(), Box<dyn std::error::Error>> {
-        let field_scale = 0.0;
-        let field_center = Vec3::new(5.0, 5.0, 0.0);
-
-        let mut root =
-            BitMapBackend::new("output/test_straight_paths.png", (640, 480)).into_drawing_area();
+    fn plot_trajectories(
+        field_scale: f32,
+        field_center: &Vec3,
+        lines: u32,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let path = format!("output/test_{}_paths.png", field_scale);
+        let mut root = BitMapBackend::new(&path, (640, 480)).into_drawing_area();
         root.fill(&WHITE)?;
         let mut chart = ChartBuilder::on(&root)
-            .caption("f=0.0", ("Arial", 50).into_font())
+            .caption(format!("f={}", field_scale), ("Arial", 50).into_font())
             .margin(5 as u32)
             .x_label_area_size(30 as u32)
             .y_label_area_size(30 as u32)
@@ -42,8 +42,10 @@ mod tests {
             },
         ))?;
 
-        for x in 0..=10 {
-            let end = Vec3::new(x as f32, 10.0, 0.0);
+        for x in 0..lines {
+            let r = (x as f32) / ((lines as f32) - 1.0);
+
+            let end = Vec3::new(10.0 * r, 10.0, 0.0);
             let ray = Ray::new(start, end - start);
             let steps = cast_ray_steps_debug(&ray, field_scale, &field_center);
 
@@ -51,15 +53,68 @@ mod tests {
             for i in 0..steps.len() {
                 let (x, y) = (steps[i].x, steps[i].y);
                 if x < 0.0 || x > 10.0 || y < 0.0 || y > 10.0 {
+                    let (prev_x, prev_y) = (steps[i - 1].x, steps[i - 1].y);
+                    let (final_x, final_y) = (x, y);
+                    let (slope_x, slope_y) = (final_x - prev_x, final_y - prev_y);
+                    let mut t = 1.0;
+                    if final_x < 0.0 {
+                        t = f32::min(-prev_x / slope_x, t);
+                    }
+                    if final_x > 10.0 {
+                        t = f32::min((10.0 - prev_x) / slope_x, t);
+                    }
+                    if final_y < 0.0 {
+                        t = f32::min(-prev_y / slope_y, t);
+                    }
+                    if final_y > 10.0 {
+                        t = f32::min((10.0 - prev_y) / slope_y, t);
+                    }
+                    debug.push((prev_x + slope_x * t, prev_y + slope_y * t));
                     break;
                 }
                 debug.push((x, y));
             }
-            chart.draw_series(LineSeries::new(debug, &RED))?;
+            let mut color = RGBColor((255.0 * r) as u8, (255.0 * (1.0 - r)) as u8, 0);
+            if steps.len() < 10000 {
+                color = BLACK;
+            }
+
+            chart.draw_series(LineSeries::new(debug, &color))?;
         }
         // And if we want SVG backend
         // let backend = SVGBackend::new("output.svg", (800, 600));
         root.present().expect("Plot failed!");
+        Ok(())
+    }
+
+    fn find_near_miss(field_scale: f32, field_center: &Vec3) -> (Vec3, Vec3) {
+        let start = Vec3::new(5.0, 0.0, 0.0);
+        let mut left = Vec3::new(0.0, 10.0, 0.0);
+        let mut right = *field_center;
+        while (left - right).length() > 0.0001 {
+            let center = 0.5 * (left + right);
+            let ray = Ray::new(start, center.normalize());
+            let steps = cast_ray_steps_debug(&ray, field_scale, &field_center);
+            if steps.len() < 10000 {
+                // hit the black hole
+                right = center;
+            } else {
+                left = center;
+            }
+        }
+        (left, right)
+    }
+
+    #[test]
+    fn plot_all_trajectories() -> Result<(), Box<dyn std::error::Error>> {
+        for scale in 0..=10 {
+            plot_trajectories((scale as f32) / 10.0, &Vec3::new(5.0, 5.0, 0.0), 100)?;
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn plot_near_trajectory() -> Result<(), Box<dyn std::error::Error>> {
         Ok(())
     }
 }
