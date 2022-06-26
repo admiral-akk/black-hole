c00dda413e3fa40724b1f6eda63df9481dc9fbd5
Kuba Karpierz <mr.karpierz@gmail.com>
2022-06-26 10:47:20 -0400

    implement anti-aliasing

--- rendering/src/render.rs
+++ rendering/src/render.rs
@@ -4,22 +4,40 @@ use structs::{black_hole::BlackHole, camera::Camera, stars::Stars};
 use crate::structs;
 
 pub fn render(camera: &mut Camera, stars: &Stars, black_hole: &BlackHole) {
-    let rays = camera.get_rays();
+    // We need to calculate the rgba value of each pixel. We can do this by:
+    // 1. iterating over x/y
+    // 2. asking the camera to generate a bunch of rays
+    // 3. asking the cache what those rays resolve to
+    // 4. recombining the values into a single rgba value.
     let cache = RayCache::compute_new(
         10000,
         &black_hole.field,
         &camera.pos,
         std::f64::consts::PI * camera.vertical_fov / 180.0,
     );
-    for i in 0..rays.len() {
-        let final_dir = cache.final_dir(&rays[i], &black_hole.field);
-        //let final_dir = black_hole.raycast(ray);
-        if final_dir.is_none() {
-            let c = [0u8, 0u8, 0u8, 255u8];
-            camera.write_color(i, &c);
-        } else {
-            let c = stars.get_rgba(&(final_dir.unwrap()));
-            camera.write_color(i, &c);
+
+    for x in 0..camera.get_dimensions().width {
+        for y in 0..camera.get_dimensions().height {
+            let rays = camera.get_rays(x, y);
+            let ray_count = rays.len();
+            let mut color = [0, 0, 0, 255];
+            for ray in rays {
+                let final_dir = cache.final_dir(&ray, &black_hole.field);
+                if final_dir.is_some() {
+                    let c = stars.get_rgba(&(final_dir.unwrap()));
+                    for i in 0..4 {
+                        color[i] += c[i] as u32;
+                    }
+                }
+            }
+            let c = [
+                (color[0] / ray_count as u32) as u8,
+                (color[1] / ray_count as u32) as u8,
+                (color[2] / ray_count as u32) as u8,
+                255,
+            ];
+
+            camera.write_color(x, y, &c);
         }
     }
 }
--- rendering/src/structs/camera.rs
+++ rendering/src/structs/camera.rs
@@ -7,6 +7,7 @@ pub struct Camera {
     dimensions: Dimensions,
     pub pos: DVec3,
     pub vertical_fov: f64,
+    aa_level: u32,
     out: Vec<u8>,
 }
 
@@ -17,6 +18,7 @@ impl Camera {
             dimensions,
             pos,
             vertical_fov,
+            aa_level: 3,
             out,
         }
     }
@@ -35,19 +37,39 @@ impl Camera {
         Ray::new(DVec3::new(self.pos.x, self.pos.y, self.pos.z), viewport)
     }
 
-    pub fn get_rays(&self) -> Vec<Ray> {
+    pub fn get_rays(&self, x: usize, y: usize) -> Vec<Ray> {
+        let y_size = f64::tan(self.vertical_fov * std::f64::consts::PI / 360.0);
+        let x_size = y_size * self.dimensions.aspect_ratio();
+
+        let (half_width, half_height) = (
+            (self.dimensions.width / 2) as f64,
+            (self.dimensions.height / 2) as f64,
+        );
+        let view_base_x = x_size * ((x as f64) - half_width) / half_width;
+        let view_base_y = y_size * ((y as f64) - half_height) / half_height;
+        let pixel_size = y_size / half_height;
+        let aa_half_step = pixel_size / ((2 * self.aa_level + 2) as f64);
+
+        let aa_count = 1 << self.aa_level;
         let mut rays = Vec::new();
-        let dimensions = &self.dimensions;
-        for y in (0..dimensions.height).rev() {
-            for x in 0..dimensions.width {
-                rays.push(self.get_ray(x, y));
+        for x in 0..aa_count {
+            for y in 0..aa_count {
+                rays.push(Ray::new(
+                    self.pos,
+                    DVec3::new(
+                        view_base_x + ((2 * x + 1) as f64) * aa_half_step,
+                        view_base_y + ((2 * y + 1) as f64) * aa_half_step,
+                        1.0,
+                    ),
+                ))
             }
         }
 
         rays
     }
 
-    pub fn write_color(&mut self, index: usize, color: &[u8; 4]) {
+    pub fn write_color(&mut self, x: usize, y: usize, color: &[u8; 4]) {
+        let index = self.dimensions.to_index(x, y);
         let c: &mut [u8] = &mut self.out[(4 * index)..(4 * index + 4)];
         c[0] = color[0];
         c[1] = color[1];
