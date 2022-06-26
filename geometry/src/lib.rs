38690bcc2eb701e2d4666cdaaca6346ea39aa569
Kuba Karpierz <mr.karpierz@gmail.com>
2022-06-25 20:27:58 -0400

    Removed utils

--- geometry/src/lib.rs
+++ geometry/src/lib.rs
@@ -5,7 +5,6 @@ pub type DVector3 = glam::DVec3;
 pub type Vec3 = Vector3;
 pub type DVec3 = DVector3;
 
-pub mod utils;
 pub use structs::disc::Disc;
 pub use structs::ray::Ray;
 pub use structs::sphere::Sphere;
--- rendering/src/structs/stars.rs
+++ rendering/src/structs/stars.rs
@@ -1,4 +1,4 @@
-use geometry::{utils::to_phi_theta, DVec3};
+use geometry::DVec3;
 use image::{DynamicImage, GenericImageView};
 
 pub struct Stars {
@@ -12,7 +12,7 @@ impl Stars {
 
     pub fn get_rgba(&self, dir: &DVec3) -> [u8; 4] {
         let mod_dir = DVec3::new(dir.x, dir.z, dir.y);
-        let (phi, theta) = to_phi_theta(&mod_dir);
+        let (phi, theta) = Stars::to_phi_theta(&mod_dir);
         let x = (self.background.width() as f64) * phi / std::f64::consts::TAU;
         let y = (self.background.height() as f64) * (theta + std::f64::consts::FRAC_PI_2)
             / std::f64::consts::PI;
@@ -22,5 +22,13 @@ impl Stars {
         );
 
         rgba.0
+    }
+
+    pub fn to_phi_theta(vec: &DVec3) -> (f64, f64) {
+        let horizontal_len = (vec.x * vec.x + vec.y * vec.y).sqrt();
+        (
+            (f64::atan2(vec.y, vec.x) + std::f64::consts::TAU) % std::f64::consts::TAU,
+            f64::atan2(vec.z, horizontal_len),
+        )
     }
 }
