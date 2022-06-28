bfe5a2502e337cb473df752bbd1f29c2767484b3
Kuba Karpierz <mr.karpierz@gmail.com>
2022-06-28 09:22:19 -0400

    Moved to using glam quaternion

--- rendering/src/structs/observer.rs
+++ rendering/src/structs/observer.rs
@@ -1,30 +1,32 @@
 use std::f64::consts::PI;
 
+use glam::f64::DQuat;
 use glam::DVec3;
 use path_integration::BlackHole;
-use quaternion::Quaternion;
-
 pub struct Observer {
     canon_forward: DVec3,
     canon_up: DVec3,
     canon_right: DVec3,
     view_width: f64,
-    from_canon: Quaternion<f64>,
+    from_canon: DQuat,
 }
 
 // We want to line up pos with -Z, up with +Y.
 // We apply this rotation to forward and right to get the rays. Then use them to generate the initial dir.
 
-fn canon_rotation(pos: DVec3, up: DVec3) -> (Quaternion<f64>, Quaternion<f64>) {
-    let target_pos = -pos.length() * DVec3::Z;
+fn canon_rotation(pos: DVec3, up: DVec3) -> (DQuat, DQuat) {
+    let pos = pos.normalize();
+    let up = up.normalize();
+
+    let target_pos = -DVec3::Z;
     let target_up = DVec3::Y;
 
-    let q1 = quaternion::rotation_from_to(pos.to_array(), target_pos.to_array());
-    let rotated_up = quaternion::rotate_vector(q1, up.to_array());
-    let q2 = quaternion::rotation_from_to(rotated_up, target_up.to_array());
-    let to_canon = quaternion::mul(q2, q1);
-    let len = quaternion::square_len(to_canon);
-    let from_canon = quaternion::scale(quaternion::conj(to_canon), 1.0 / len);
+    let q1 = DQuat::from_rotation_arc(pos, target_pos);
+    let rotated_up = q1 * up;
+    let q2 = DQuat::from_rotation_arc(rotated_up, target_up);
+    let to_canon = q2 * q1;
+    let from_canon = to_canon.inverse();
+
     (to_canon, from_canon)
 }
 
@@ -32,8 +34,7 @@ impl Observer {
     pub fn new(pos: DVec3, forward: DVec3, up: DVec3, vertical_fov_degrees: f64) -> Self {
         let (to_canon, from_canon) = canon_rotation(pos, up);
         let forward = forward.normalize();
-        let canon_forward =
-            DVec3::from_array(quaternion::rotate_vector(to_canon, forward.to_array())).normalize();
+        let canon_forward = (to_canon * forward).normalize();
         let view_mag = 2.0 * f64::tan(PI * vertical_fov_degrees / 360.0);
         let canon_up = DVec3::Y;
         let canon_right = canon_forward.cross(canon_up).normalize();
@@ -67,8 +68,7 @@ impl Observer {
 
     fn to_final_dir_transform(&self, canon: &DVec3, dir: &DVec3) -> DVec3 {
         let angle = f64::atan2(canon.y, -canon.x);
-        let final_rot = quaternion::euler_angles(0.0, 0.0, -angle);
-        let inv = quaternion::rotate_vector(final_rot, dir.to_array());
-        DVec3::from_array(quaternion::rotate_vector(self.from_canon, inv))
+        let first_rot = DQuat::from_rotation_z(-angle);
+        self.from_canon * first_rot * *dir
     }
 }
