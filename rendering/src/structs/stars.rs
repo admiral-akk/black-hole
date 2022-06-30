40a648ba7c34f99a26ab3886d383f5177e3f8d46
Kuba Karpierz <mr.karpierz@gmail.com>
2022-06-30 09:38:30 -0400

    Updated stars to cache multiplier values and only use 32 bit variables

--- rendering/src/structs/stars.rs
+++ rendering/src/structs/stars.rs
@@ -1,4 +1,4 @@
-use std::f32::consts::{FRAC_1_PI, TAU};
+use std::f32::consts::{FRAC_1_PI, FRAC_PI_2, TAU};
 
 use glam::Vec3;
 use image::{DynamicImage, GenericImageView};
@@ -10,35 +10,44 @@ use super::{data::Data, polar_coordinates::PolarCoordinates};
 pub struct Stars {
     background: DynamicImage,
     offset: PolarCoordinates,
+    scaling: ScalingFactors,
+}
+
+struct ScalingFactors {
+    pub background_width_f32: f32,
+    pub background_height_f32: f32,
 }
 
 const FRAC_1_TAU: f32 = 1.0 / TAU;
 
 impl Stars {
     pub fn new(background: DynamicImage) -> Stars {
+        let (width, height) = background.dimensions();
         Self {
             background,
             offset: PolarCoordinates {
                 phi: 0.0,
-                theta: 0.0,
+                theta: FRAC_PI_2,
+            },
+            scaling: ScalingFactors {
+                background_width_f32: (width as f32) * FRAC_1_TAU,
+                background_height_f32: (height as f32) * FRAC_1_PI,
             },
         }
     }
 
     pub fn update_position(&mut self, pos: &Vec3) {
         self.offset = pos.to_polar();
+        self.offset.theta += FRAC_PI_2;
     }
 
     pub fn to_rgba(&self, data: &mut Vec<Data>) {
         for sample in data.iter_mut() {
             match sample {
                 Data::Polar(index, polar) => {
-                    let x_image = FRAC_1_TAU
-                        * (self.background.width() as f32)
-                        * (self.offset.phi as f32 + polar.phi as f32);
-                    let y_image = FRAC_1_PI
-                        * (self.background.height() as f32)
-                        * ((self.offset.theta + polar.theta) + std::f32::consts::FRAC_PI_2);
+                    let x_image = self.scaling.background_width_f32 * (self.offset.phi + polar.phi);
+                    let y_image =
+                        self.scaling.background_height_f32 * (self.offset.theta + polar.theta);
                     let rgba = self.background.get_pixel(
                         (x_image as u32) % self.background.width(),
                         (y_image as u32) % self.background.height(),
@@ -52,8 +61,8 @@ impl Stars {
 
     pub fn get_rgba(&self, dir: &Vec3) -> [u8; 4] {
         let polar = PolarCoordinates::new(&dir);
-        let x = (self.background.width() as f64) * (polar.phi as f64) / std::f64::consts::TAU;
-        let y = (self.background.height() as f64) * (polar.theta as f64) / std::f64::consts::PI;
+        let x = self.scaling.background_width_f32 * polar.phi;
+        let y = self.scaling.background_height_f32 * polar.theta;
         let rgba = self.background.get_pixel(
             (x as u32) % self.background.width(),
             (y as u32) % self.background.height(),
