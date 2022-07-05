bd973fbab4a09f7d05174e515c67f3fb073fcd89
Kuba Karpierz <mr.karpierz@gmail.com>
2022-07-05 15:13:48 -0400

    Moved horizontal blur to use define

--- web/src/shaders/fragment/horizontal_blur.glsl
+++ web/src/shaders/fragment/horizontal_blur.glsl
@@ -1,12 +1,12 @@
 #version 300 es
 
 precision mediump float;
-uniform float w[3];
+uniform float w[K];
 uniform sampler2D rtt_sampler;
 out vec4 outColor;
 void main(){
     outColor=w[0]*texture(rtt_sampler,(gl_FragCoord.xy+vec2(0,0))/512.);
-    for(int i=1;i<=2;i++){
+    for(int i=1;i<K;i++){
         outColor+=w[i]*texture(rtt_sampler,(gl_FragCoord.xy+vec2(-i,0))/512.);
         outColor+=w[i]*texture(rtt_sampler,(gl_FragCoord.xy+vec2(i,0))/512.);
     }
